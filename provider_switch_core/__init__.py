5df06e947200d44dd521848ff5e6f447003dd97a
denizw030 <denizwnm@gmail.com>
2026-08-26 01:39:06 +0200

    feat(provider-core): export provider integration interfaces

--- provider_switch_core/__init__.py
+++ provider_switch_core/__init__.py
@@ -1,6 +1,7 @@
 from .models import *
 from .costs import calculate_cost, first_and_following_year
 from .ranking import rank_offers, WEIGHTS, RANKING_VERSION
-from .providers import ProviderAdapter, FixtureProviderAdapter, VerivoxAdapter, Check24Adapter, DirectProviderAdapter
+from .providers import *
 from .validation import validate_contract, validate_offer
 from .safety import safety_gate, execution_allowed
+from .recommendations import select_concierge_recommendations
