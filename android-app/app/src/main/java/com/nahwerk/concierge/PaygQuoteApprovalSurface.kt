f3a5feee16dafaf02f6543cf81c5403fe17421b9
denizw030 <denizwnm@gmail.com>
2026-09-12 17:18:18 +0200

    Android: fix PAYG approval surface compile import

--- android-app/app/src/main/java/com/nahwerk/concierge/PaygQuoteApprovalSurface.kt
+++ android-app/app/src/main/java/com/nahwerk/concierge/PaygQuoteApprovalSurface.kt
@@ -26,6 +26,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import com.nahwerk.concierge.data.PaygQuote
 import com.nahwerk.concierge.data.PaygQuoteActionApi
 import com.nahwerk.concierge.data.PaygSnapshot
