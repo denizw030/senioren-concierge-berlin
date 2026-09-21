dd78904db6e49cdb9fd1fa6130d031cb0ab0c3c5
denizw030 <denizwnm@gmail.com>
2026-09-21 03:10:25 +0200

    Align email actions with portal theme control

--- assets/site-ui.js
+++ assets/site-ui.js
@@ -209,6 +209,8 @@
         </label>
       `;
       tabsShell.insertAdjacentElement('afterend', setting);
+      const emailActions = document.getElementById('emailTopActions');
+      if (emailActions) setting.prepend(emailActions);
 
       const toggle = setting.querySelector('#nwPortalThemeToggle');
       const syncToggle = () => {
