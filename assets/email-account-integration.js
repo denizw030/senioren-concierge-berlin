92acb0e3c4399d7f3c5b1206da5700000a21833f
denizw030 <denizwnm@gmail.com>
2026-09-24 23:48:10 +0200

    Refresh reader toolbar asset cache in assets/email-account-integration.js

--- assets/email-account-integration.js
+++ assets/email-account-integration.js
@@ -174,7 +174,7 @@
       const link = document.createElement("link");
       link.id = "emailConciergeProductStyles";
       link.rel = "stylesheet";
-      link.href = "/assets/email-concierge-product.css?v=20260924-compose3";
+      link.href = "/assets/email-concierge-product.css?v=20260924-reader4";
       document.head.appendChild(link);
     }
     productAssetPromise = new Promise((resolve) => {
