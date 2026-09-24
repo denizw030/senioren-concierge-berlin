85959025ce20611bc4f3502ddb77484e23b1cd63
denizw030 <denizwnm@gmail.com>
2026-09-24 23:20:03 +0200

    Refresh composer button asset cache in assets/email-account-integration.js

--- assets/email-account-integration.js
+++ assets/email-account-integration.js
@@ -174,7 +174,7 @@
       const link = document.createElement("link");
       link.id = "emailConciergeProductStyles";
       link.rel = "stylesheet";
-      link.href = "/assets/email-concierge-product.css?v=20260924-compose2";
+      link.href = "/assets/email-concierge-product.css?v=20260924-compose3";
       document.head.appendChild(link);
     }
     productAssetPromise = new Promise((resolve) => {
